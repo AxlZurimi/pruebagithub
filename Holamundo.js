deb8c7c161a505f77718e83ced29ac26c297e381
AxlZurimi <axel_hm@tesch.edu.mx>
2024-05-30 19:26:01 -0600

    Update Holamundo.js
    put juendioego

--- Holamundo.js
+++ Holamundo.js
@@ -1,7 +1,9 @@
 let nombre = 'Kevin';
 let a = 10;
 let b = 20;
-let c = a + b;
+let c = a + b;cmwklegnklnegk,n,.
 console.log(`hola ` + nombre);
 console.log(`hola ${nombre}`);
 console.log(`la suma de a+b es ${c}`);
+  wjqdg  ugfqjgjh
+elsapatoooooo
